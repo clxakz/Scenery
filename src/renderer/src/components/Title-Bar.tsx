f1a5f451396369d3c3171c29c0a7576f0a46e7a6
= <=>
2025-12-30 18:26:56 +0100

    UI Improvements

--- src/renderer/src/components/Title-Bar.tsx
+++ src/renderer/src/components/Title-Bar.tsx
@@ -11,16 +11,16 @@ export default function TitleBar() {
 	}, []);
 
 	return (
-		<div className="h-6.5 w-full border-b bg-secondary window-drag flex items-center px-1">
-			{progress === null ? (
-				<p className="text-xs text-muted-foreground">{window.ipcRenderer.getAppVersion()}</p>
-			) : progress >= 100 ? (
-				<p className="text-xs text-muted-foreground">restart app to update</p>
-			) : (
-				<p className="text-xs text-muted-foreground">{Math.floor(progress)}% downloading update</p>
-			)}
+		<div className="h-6.5 w-full border-b bg-secondary window-drag flex items-center justify-center relative">
+			<p className="text-xs text-muted-foreground absolute left-1">
+				{progress === null
+					? `${window.ipcRenderer.getAppVersion()}`
+					: progress >= 100
+						? "restart app to update"
+						: `${Math.floor(progress)}% downloading update`}
+			</p>
 
-			<p className="flex justify-center flex-1 text-sm">scenery</p>
+			<p className="text-sm">scenery</p>
 		</div>
 	);
 }
